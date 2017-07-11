a25c5cb7bf78f15cb035d7d1d6bea57c9f2db881
root <root@ip-172-31-20-186.us-west-2.compute.internal>
2017-07-11 07:14:29 +0000

    timer commit

--- timer.sh
+++ timer.sh
@@ -3,6 +3,6 @@ while sleep 10
 do
 mysqldump -u root -p8111249 blog > ./blogbackup.sql
 sudo git add .
-sudo git commit -m \"timer commit\"
+sudo git commit -m "timer commit"
 sudo git push origin master
 done
